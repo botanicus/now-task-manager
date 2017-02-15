641d821e5f1eba5c9c7c451e63befe1468f55de9
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-02-15 14:35:27 -0500

    Punch off.

--- lib/pomodoro/manager.rb
+++ lib/pomodoro/manager.rb
@@ -21,12 +21,34 @@ module Pomodoro
       @tasks[:today]
     end
 
+    def tasks_for_later
+      @tasks[:later]
+    end
+
     def mark_active_task_as_done
+      Time.now.strftime('%H:%M')
       self.active_task.tags.push(:done)
     end
 
     def active_task
       self.today_tasks.find { |task| ! task.tags.include?(:done) }
+    end
+
+    def add_task_for_later(task)
+      @tasks[:later] << Task.new(task)
+    end
+
+    def finished_tasks
+      self.today_tasks.select { |task| task.tags.include?(:done) }
+    end
+
+    def switch_days
+      unfinished_tasks = @tasks[:today].select { |task| ! task.tags.include?(:done) }
+      @tasks[:today] = @tasks[:template].dup
+      placeholder = @tasks[:today].find { |task| task.text == '[content]' }
+      position = @tasks[:today].index(placeholder)
+      @tasks[:today].delete(placeholder)
+      @tasks[:today].insert(position, *unfinished_tasks)
     end
 
     def save(stream = File.open(@task_list_path, 'w'))

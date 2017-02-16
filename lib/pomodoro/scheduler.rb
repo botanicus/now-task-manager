e95913ddb12cb1374be00ea7fd6dfc63b0f9ac07
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-02-16 14:01:17 -0500

    Date exts.

--- lib/pomodoro/scheduler.rb
+++ lib/pomodoro/scheduler.rb
@@ -3,8 +3,8 @@ require 'pomodoro/schedule/dsl'
 
 module Pomodoro
   class Scheduler
-    def self.load(schedule_path)
-      context = Pomodoro::Schedule::DSL.new
+    def self.load(schedule_path, today = Date.today)
+      context = Pomodoro::Schedule::DSL.new(today)
       context.instance_eval(File.read(schedule_path))
       self.new(context)
     end
@@ -13,7 +13,7 @@ module Pomodoro
       @schedule = schedule
     end
 
-    def for_today(today = Date.today, debug = false)
+    def for_today(debug = false)
       Array.new.tap do |tasks|
         @schedule.rules.each do |name, rule|
           puts "~ Rule #{name} evaluated to #{rule.true?}" if debug

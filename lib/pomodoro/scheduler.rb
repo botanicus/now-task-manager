a3103b4b419c638ba8a95214984524039a446170
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2017-11-28 22:39:38 +0100

    Time framing in progress.

--- lib/pomodoro/scheduler.rb
+++ lib/pomodoro/scheduler.rb
@@ -9,7 +9,7 @@ module Pomodoro
 
     def self.load(schedule_path, today = Date.today)
       schedule_dir = File.expand_path("#{schedule_path}/..")
-      context = Pomodoro::Schedule::DSL.new(today, schedule_dir)
+      context = Pomodoro::Schedule::DSL.new(schedule_dir, today)
       context.instance_eval(File.read(schedule_path), schedule_path)
       self.new(context)
     end
@@ -18,9 +18,17 @@ module Pomodoro
       @schedule = schedule
     end
 
+    def rules
+      @schedule.rules
+    end
+
+    def schedules
+      @schedule.schedules
+    end
+
     def for_today(debug = false)
       Array.new.tap do |tasks|
-        @schedule.rules.each do |name, rule|
+        self.rules.each do |name, rule|
           puts "~ Rule #{name} evaluated to #{rule.true?}" if debug
           rule.call(tasks) if rule.true?
         end
--- lib/pomodoro/time_frame.rb
+++ lib/pomodoro/time_frame.rb
@@ -4,12 +4,34 @@ module Pomodoro
   class TimeFrame
     ALLOWED_OPTIONS ||= [:online, :writeable, :note, :tags]
 
+    attr_reader :name, :tasks, :interval, :options
     def initialize(name, tag, interval_from, interval_to, options = Hash.new)
       @name, @tag, @options = name, tag, options
-      @interval = [Hour.parse(interval_from), Hour.parse(interval_to)]
+      @interval = [interval_from && Hour.parse(interval_from), interval_to && Hour.parse(interval_to)]
+      @tasks = Array.new
 
       unless (unrecognised_options = options.keys - ALLOWED_OPTIONS).empty?
         raise ArgumentError.new("Unrecognised options: #{unrecognised_options.inspect}")
+      end
+    end
+
+    def header
+      if @interval[0] && @interval[1]
+        "#{@name} (#{@interval[0]} – #{@interval[1]})"
+      elsif @interval[0] && ! @interval[1]
+        "#{@name} (from #{@interval[0]})"
+      elsif ! @interval[0] && @interval[1]
+        raise "I don't think this makes sense."
+      else
+        @name
+      end
+    end
+
+    def to_s
+      if @tasks.empty?
+        self.header
+      else
+        ["#{self.header}:", self.tasks.map(&:to_s)].join("\n")
       end
     end
 

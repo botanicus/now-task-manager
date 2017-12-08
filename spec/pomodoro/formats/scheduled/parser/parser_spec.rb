7d26211f6d565642e74c2639895df8cd4760c061
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-12-08 13:21:52 +0100

    Scheduled format done.

--- spec/pomodoro/formats/scheduled/parser/parser_spec.rb
+++ spec/pomodoro/formats/scheduled/parser/parser_spec.rb
@@ -1,4 +1,78 @@
 require 'pomodoro/formats/scheduled'
+require 'spec_helper'
 
 describe Pomodoro::Formats::Scheduled::Parser do
+  describe 'rules' do
+    describe 'task_group_header' do
+      it "parses anything followed by a new line" do
+        expect {
+          subject.task_group_header.parse("Tomorrow\n")
+        }.not_to raise_error
+      end
+    end
+
+    describe 'task' do
+      it "parses anything that starts with a dash" do
+        expect {
+          subject.task.parse("- [9:20] Buy shoes #errands\n")
+        }.not_to raise_error
+      end
+    end
+
+    describe 'task_group' do
+      it "parses task_group_header followed by any number of tasks" do
+        expect {
+          subject.task_group.parse("Tomorrow\n- Buy shoes #errands\n- [9:20] Call Tom.\n")
+        }.not_to raise_error
+      end
+    end
+  end
+
+  describe '#parse' do
+    it "parses an empty file" do
+      expect { subject.parse('') }.not_to raise_error
+    end
+
+    let(:task_group_1) do
+      "Tomorrow\n- Buy shoes #errands\n- [9:20] Call Tom.\n"
+    end
+
+    let(:task_group_2) do
+      "Prague\n- Task 1.\n"
+    end
+
+    let(:task_groups) do
+      [task_group_1, task_group_2].join("\n")
+    end
+
+    it "parses one task group" do
+      expect { subject.parse(task_group_1) }.not_to raise_error
+    end
+
+    it "parses multiple task groups" do
+      expect { subject.parse(task_groups) }.not_to raise_error
+    end
+
+    it "returns a tree" do
+      tree = subject.parse(task_groups)
+      expect(tree).to eql([
+        {
+          task_group: {
+            task_group_header: 'Tomorrow',
+            task_list: [
+              {task: "Buy shoes #errands"},
+              {task: "[9:20] Call Tom."}
+            ]
+          }
+        }, {
+          task_group: {
+            task_group_header: 'Prague',
+            task_list: [
+              {task: "Task 1."}
+            ]
+          }
+        }
+      ])
+    end
+  end
 end
--- spec/pomodoro/formats/scheduled/parser/transformer_spec.rb
+++ spec/pomodoro/formats/scheduled/parser/transformer_spec.rb
@@ -1,4 +1,37 @@
 require 'pomodoro/formats/scheduled'
 
 describe Pomodoro::Formats::Scheduled::Transformer do
+  let(:tree) do # Copied from the parser_spec.rb.
+    [
+      {
+        task_group: {
+          task_group_header: 'Tomorrow',
+          task_list: [
+            {task: "Buy shoes #errands"},
+            {task: "[9:20] Call Tom."}
+          ]
+        }
+      }, {
+        task_group: {
+          task_group_header: 'Prague',
+          task_list: [
+            {task: "Task 1."}
+          ]
+        }
+      }
+    ]
+  end
+
+  describe '#apply' do
+    it do
+      ast = subject.apply(tree)
+      expect(ast.length).to be(2)
+
+      expect(ast[0].name).to eql('Tomorrow')
+      expect(ast[0].tasks).to eql(["Buy shoes #errands", "[9:20] Call Tom."])
+
+      expect(ast[1].name).to eql('Prague')
+      expect(ast[1].tasks).to eql(['Task 1.'])
+    end
+  end
 end

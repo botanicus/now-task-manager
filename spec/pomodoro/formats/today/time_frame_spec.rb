5a51705ff85924387b4faa9ba9730974b563bcca
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2017-12-10 21:48:10 +0100

    More validations, more specs.

--- spec/pomodoro/formats/today/time_frame_spec.rb
+++ spec/pomodoro/formats/today/time_frame_spec.rb
@@ -13,6 +13,28 @@ describe Pomodoro::Formats::Today::TimeFrame do
         ArgumentError, /At least one of start_time and end_time has to be provided./)
     end
 
+    it "fails if start time or end time is not an Hour instance" do
+      expect {
+        described_class.new(name: 'X', start_time: Time.now, tasks: Hash.new)
+      }.to raise_error(ArgumentError, /Start time and end time has to be an Hour instance/)
+
+      expect {
+        described_class.new(name: 'X', end_time: 30, tasks: Hash.new)
+      }.to raise_error(ArgumentError, /Start time and end time has to be an Hour instance/)
+    end
+
+    it "fails if tasks is not an array" do
+      expect {
+        described_class.new(name: 'X', start_time: Hour.parse('7:50'), tasks: Hash.new)
+      }.to raise_error(ArgumentError, /Data is supposed to be an array of Task instances/)
+    end
+
+    it "fails if tasks is not an array of task-like objects" do
+      expect {
+        described_class.new(name: 'X', start_time: Hour.parse('7:50'), tasks: [Object.new])
+      }.to raise_error(ArgumentError, /Data is supposed to be an array of Task instances/)
+    end
+
     it "succeeds when name and start_time is provided" do
       time_frame = described_class.new(name: 'Morning routine', start_time: Hour.parse('7:50'))
       expect(time_frame.name).to eql('Morning routine')
@@ -40,7 +62,7 @@ describe Pomodoro::Formats::Today::TimeFrame do
         described_class.new(name: 'Morning routine', start_time: Hour.parse('7:50'))
       end
 
-      it "returns a valid scheduled task list formatted string" do
+      it "returns a valid today task list formatted string" do
         expect(subject.to_s).to eql("Morning routine (from 7:50)\n")
       end
     end

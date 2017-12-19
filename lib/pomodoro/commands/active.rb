6815b834c68abfce87fe858072a999ae8b137e41
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-12-19 09:59:51 +0100

    Add silent option to active.

--- lib/pomodoro/commands/active.rb
+++ lib/pomodoro/commands/active.rb
@@ -3,18 +3,21 @@ class Pomodoro::Commands::Active < Pomodoro::Commands::Command
 
   self.help = <<-EOF.gsub(/^\s*/, '')
     now <green>active</green> '%b' <bright_black># Print the active task.</bright_black>
+    now <green>active</green> -s '%b' <bright_black># Print the active task.</bright_black>
   EOF
 
   def run
     must_exist(self.config.today_path)
     today_list = parse_today_list(self.config)
 
+    silent = @args.delete('-s')
+
     if active_task = today_list.active_task
       format_string = (@args.shift || '%b').dup
       format_string.gsub!('%b', active_task.body)
       # TODO ...
       puts format_string
-    else
+    elsif ! silent
       abort "<red>No active tasks.</red>".colourise
     end
   end

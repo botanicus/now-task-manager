8b889943e41856b5ca19e490e8d869a5d83b99c4
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-12-20 20:10:06 +0100

    Some failing specs fixed.

--- spec/pomodoro/formats/scheduled/parser/transformer_spec.rb
+++ spec/pomodoro/formats/scheduled/parser/transformer_spec.rb
@@ -6,17 +6,34 @@ describe Pomodoro::Formats::Scheduled::Transformer do
     [
       {
         task_group: {
-          header: 'Tomorrow',
-          task_list: [
-            {task: "Buy shoes #errands"},
-            {task: "[9:20] Call Tom."}
+          header: {str: 'Tomorrow'},
+          tasks: [
+            {
+              task: {
+                body: {str: "Buy shoes. "},
+                tags: [
+                  {tag: {str: 'errands'}}
+                ]
+              },
+            }, {
+              task: {
+                start_time: {hour: '9:20'},
+                body: {str: "Call Tom."},
+                tags: []
+              }
+            }
           ]
         }
       }, {
         task_group: {
-          header: 'Prague',
-          task_list: [
-            {task: "Task 1."}
+          header: {str: 'Prague'},
+          tasks: [
+            {
+              task: {
+                body: {str: "Task 1."},
+                tags: []
+              }
+            }
           ]
         }
       }
@@ -29,10 +46,28 @@ describe Pomodoro::Formats::Scheduled::Transformer do
       expect(ast.length).to be(2)
 
       expect(ast[0].header).to eql('Tomorrow')
-      expect(ast[0].tasks).to eql(["Buy shoes #errands", "[9:20] Call Tom."])
+
+      expect(ast[0].tasks.length).to be(2)
+      tasks = ast[0].tasks
+
+      expect(tasks[0].body).to eql("Buy shoes.")
+      expect(tasks[0].tags).to eql([:errands])
+      expect(tasks[0].start_time).to be_nil
+      expect(tasks[0].time_frame).to be_nil
+
+      expect(tasks[1].body).to eql("Call Tom.")
+      expect(tasks[1].tags).to be_empty
+      expect(tasks[1].start_time).to eql(h('9:20'))
+      expect(tasks[1].time_frame).to be_nil
 
       expect(ast[1].header).to eql('Prague')
-      expect(ast[1].tasks).to eql(['Task 1.'])
+
+      expect(ast[1].tasks.length).to be(1)
+      task = ast[1].tasks[0]
+      expect(task.body).to eql("Task 1.")
+      expect(task.tags).to be_empty
+      expect(task.start_time).to be_nil
+      expect(task.time_frame).to be_nil
     end
   end
 end

9f6d33e82369eabb56356cbcfa71b2ab9ab310b2
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-12-21 12:32:23 +0100

    Testing commands.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,38 @@ class Parslet::Slice
   end
 end
 
+module CLITestHelpers
+  def self.extended(base)
+    base.define_singleton_method(:sequence) do
+      @sequence ||= Array.new
+    end
+  end
+
+  def p(object)
+    self.sequence << {p: object}
+  end
+
+  def puts(message)
+    self.sequence << {stdout: message}
+  end
+
+  def warn(message)
+    self.sequence << {warn: message}
+  end
+
+  def abort(message)
+    self.sequence << {abort: message}
+  end
+
+  def command(command)
+    self.sequence << {command: message}
+  end
+
+  def exit(value = nil)
+    self.sequence << {exit: value}
+  end
+end
+
 RSpec.configure do |config|
   config.include Module.new {
     def h(string)

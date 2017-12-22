50a7ec97f283fe88f527d6495b4c67eb2d5f94f9
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2017-12-22 22:39:00 +0100

    Add command specs.

--- lib/pomodoro/commands/add.rb
+++ lib/pomodoro/commands/add.rb
@@ -7,8 +7,13 @@ class Pomodoro::Commands::Add < Pomodoro::Commands::Command
 
   def run
     task_list = parse_task_list(self.config)
-    task_list['Later'] ||= Array.new
-    task_list['Later'] << @args.join(' ')
+
+    task = Pomodoro::Formats::Scheduled::Task.new(body: @args.join(' '))
+    task_list['Later'] || task_list << Pomodoro::Formats::Scheduled::TaskGroup.new(header: 'Later')
+    task_list['Later'] << task
+
     task_list.save(self.config.task_list_path)
+  rescue Pomodoro::Config::ConfigFileMissingError => error
+    abort "<red>#{error.message}</red>"
   end
 end

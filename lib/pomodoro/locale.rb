c135b8be1b5dddf697640ca8887846f3f3624645
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2017-12-24 01:32:41 +0100

    Fix path.

--- lib/pomodoro/locale.rb
+++ lib/pomodoro/locale.rb
@@ -1,4 +1,5 @@
 require 'i18n'
 
-I18n.backend.store_translations(:en, YAML.load(File.read('i18n/en.yml')))
+path = File.expand_path('../../../i18n/en.yml', __FILE__)
+I18n.backend.store_translations(:en, YAML.load(File.read(path)))
 I18n.default_locale = :en

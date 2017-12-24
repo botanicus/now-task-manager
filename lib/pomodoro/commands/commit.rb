602ad96b8c5a1661c0cef4b997a0db2d75f7e259
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2017-12-24 03:09:40 +0100

    Deprecate bin/git-commit-current-task and replace it with now commit.

---
+++ lib/pomodoro/commands/commit.rb
@@ -0,0 +1,22 @@
+class Pomodoro::Commands::Done < Pomodoro::Commands::Command
+  self.help = <<-EOF.gsub(/^\s*/, '')
+    now <magenta>commit</magenta> <bright_black># ...</bright_black>
+    now commit -a|-v
+    now commit spec
+      For this to work you have to be in the right directory.
+  EOF
+
+  def run
+    ensure_today
+
+    if  with_active_task(self.config) do |active_task|
+          arguments = [*@args, '-m', "'#{active_task.body}'"].join(' ')
+          command("git commit #{arguments}")
+        end
+    else
+      abort "<red>There is no task in progress.</red>"
+    end
+  rescue Pomodoro::Config::ConfigError => error
+    abort "<red>#{error.message}</red>"
+  end
+end

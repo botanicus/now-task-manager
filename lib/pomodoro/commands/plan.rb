ef8441797981256f56e8267b95b93dffd3c465fe
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2017-12-26 17:11:00 -0500

    Refactoring.

--- lib/pomodoro/commands/plan.rb
+++ lib/pomodoro/commands/plan.rb
@@ -6,40 +6,17 @@ class Pomodoro::Commands::Plan < Pomodoro::Commands::Command
     now <magenta>plan</magenta> -1 [<cyan>year</cyan>|<cyan>quarter</cyan>|<cyan>month</cyan>|<cyan>week</cyan>] <bright_black># Open plan for the current time period.</bright_black>
   EOF
 
-  def week_plan_path
-    date = @args.include?('-1') ? Date.today : Date.today + 7
-    @router = Pomodoro::Router.new(self.config.data_root_path, date)
-    @router.week_plan_path
-  end
-
-  def month_plan_path
-    date = @args.include?('-1') ? Date.today : Date.today.next_month
-    @router = Pomodoro::Router.new(self.config.data_root_path, date)
-    @router.month_plan_path
-  end
-
-  def quarter_plan_path
-    date = @args.include?('-1') ? Date.today : Date.today.next_month.next_month.next_month
-    @router = Pomodoro::Router.new(self.config.data_root_path, date)
-    @router.quarter_plan_path
-  end
-
-  def year_plan_path
-    date = @args.include?('-1') ? Date.today : Date.today.next_year
-    @router = Pomodoro::Router.new(self.config.data_root_path, date)
-    @router.year_plan_path
-  end
-
   def run
-    period = @args.shift
+    period, date = self.get_period_and_date(:week)
+    router = Pomodoro::Router.new(self.config.data_root_path, date)
 
-    if self.respond_to?(:"#{period}_plan_path")
-      path = self.send(:"#{period}_plan_path")
-      unless @router.features_path.directory?
-        abort "<red>Features</red> in #{Pomodoro::Tools.format_path(@router.features_path.to_s)} are not initialised."
+    if router.respond_to?(:"#{period}_plan_path")
+      path = router.send(:"#{period}_plan_path")
+      unless router.features_path.directory?
+        abort "<red>Features</red> in #{Pomodoro::Tools.format_path(router.features_path.to_s)} are not initialised."
       end
 
-      Dir.chdir(@router.features_path.to_s) do
+      Dir.chdir(router.features_path.to_s) do
         unless File.directory?(File.dirname(path))
           command("mkdir -p #{File.dirname(path)}")
         end
--- lib/pomodoro/commands/review.rb
+++ lib/pomodoro/commands/review.rb
@@ -3,18 +3,23 @@ require 'pomodoro/router'
 class Pomodoro::Commands::Review < Pomodoro::Commands::Command
   self.help = <<-EOF.gsub(/^\s*/, '')
     now <magenta>review</magenta>                                    <bright_black># Journal for today.</bright_black>
-    now <magenta>review</magenta> [<cyan>year</cyan>|<cyan>quarter</cyan>|<cyan>month</cyan>|<cyan>week</cyan>|<cyan>today</cyan>] <bright_black>   # Review the current time period.</bright_black>
-    now <magenta>review</magenta> [<cyan>year</cyan>|<cyan>quarter</cyan>|<cyan>month</cyan>|<cyan>week</cyan>|<cyan>today</cyan>] <red.bold>-1</red.bold> <bright_black># Open yesterday's review.</bright_black>
+    now <magenta>review</magenta> [<cyan>year</cyan>|<cyan>quarter</cyan>|<cyan>month</cyan>|<cyan>week</cyan>|<cyan>day</cyan>] <bright_black>   # Review the current time period.</bright_black>
+    now <magenta>review</magenta> [<cyan>year</cyan>|<cyan>quarter</cyan>|<cyan>month</cyan>|<cyan>week</cyan>|<cyan>day</cyan>] <red.bold>-1</red.bold> <bright_black># Open yesterday's review.</bright_black>
   EOF
 
   def run
-    @date = self.get_date
-    router = Pomodoro::Router.new(self.config.data_root_path, @date)
-    period = @args.shift
+    period, date = self.get_period_and_date(:day)
+    router = Pomodoro::Router.new(self.config.data_root_path, date)
+
+    if date > Date.today
+      abort "Date cannot be past today, unless you're Doctor Who."
+    end
+
+    router = Pomodoro::Router.new(self.config.data_root_path, date)
 
     # TODO: refactor this.
-    if period.nil? || period == 'today'
-      exec("vim #{self.config.today_path(@date).sub(/\.today$/, '_review.md')}")
+    if period.nil? || period == 'day'
+      exec("vim #{self.config.today_path(date).sub(/\.today$/, '_review.md')}")
     end
 
     if router.respond_to?(:"#{period}_review_path")

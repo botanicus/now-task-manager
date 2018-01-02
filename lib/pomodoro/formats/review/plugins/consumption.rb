09e15db92e28e31b4025bdd29d7dfa22a2ff3d75
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2018-01-02 16:25:11 -0500

    WIP #94

--- lib/pomodoro/formats/review/plugins/consumption.rb
+++ lib/pomodoro/formats/review/plugins/consumption.rb
@@ -11,15 +11,56 @@ module Pomodoro::Formats::Review::Plugins::Consumption
   def self.parse(string)
     tree = Parser.new.parse(string)
     nodes = Transformer.new.apply(tree)
-    Expenses.new(nodes.empty? ? Array.new : nodes)
+    ConsumptionItems.new(nodes.empty? ? Array.new : nodes)
   end
 
   class Parser < Parslet::Parser
-    # TODO
+    rule(:float) {
+      match['\d'].repeat(1) >> (str('.') >> match['\d'].repeat(1, 1)).maybe
+    }
+
+    rule(:description) {
+      (str("\n").absent? >> any).repeat.as(:str) >> str("\n")
+    }
+
+    rule(:node) {
+      str('- ') >> float.as(:float).as(:units) >> description.as(:description)
+    }
+
+    rule(:nodes) {
+      node.as(:item).repeat
+    }
+
+    root(:nodes)
+  end
+
+  class ConsumptionItem
+    attr_reader :units, :description
+    def initialize(units:, description:)
+      @units, @description = units, description
+    end
+  end
+
+  class ConsumptionItems
+    attr_reader :items
+    def initialize(items)
+      @items = items
+    end
+
+    def sum
+      @items.sum(&:units)
+    end
   end
 
   class Transformer < Parslet::Transform
-    # TODO
+    rule(str: simple(:slice)) { slice.to_s.strip }
+    rule(float: simple(:amount_string)) do
+      amount_string.to_f
+    end
+
+    rule(item: subtree(:data)) do
+      ConsumptionItem.new(**data)
+    end
   end
 end
 

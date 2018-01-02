15019ce829a718f2c1617695ed860beef5f53a9e
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2018-01-02 08:20:45 -0500

    Base of the review parser #94.

---
+++ lib/pomodoro/formats/review.rb
@@ -0,0 +1,11 @@
+require 'parslet'
+
+module Pomodoro
+  module Formats
+    # {include:file:doc/formats/review.md}
+    module Review
+    end
+  end
+end
+
+require 'pomodoro/formats/review/parser'

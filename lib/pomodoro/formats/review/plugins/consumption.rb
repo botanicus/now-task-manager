eafd1efc1bd3310ed18f09ec3a6c93d272a5f78b
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2018-01-02 13:18:39 -0500

    WIP #94.

--- lib/pomodoro/formats/review/plugins/consumption.rb
+++ lib/pomodoro/formats/review/plugins/consumption.rb
@@ -6,4 +6,21 @@
 # ```
 
 module Pomodoro::Formats::Review::Plugins::Consumption
+  HEADER ||= 'Consumption'
+
+  def self.parse(string)
+    tree = Parser.new.parse(string)
+    nodes = Transformer.new.apply(tree)
+    Expenses.new(nodes.empty? ? Array.new : nodes)
+  end
+
+  class Parser < Parslet::Parser
+    # TODO
+  end
+
+  class Transformer < Parslet::Transform
+    # TODO
+  end
 end
+
+Pomodoro::Formats::Review::Section.register(Pomodoro::Formats::Review::Plugins::Consumption)

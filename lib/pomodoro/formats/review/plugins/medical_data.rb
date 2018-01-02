8b4b11b428fdf26b5f2823eac5d65ce80a3744af
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2018-01-02 16:14:49 -0500

    WIP #94

--- lib/pomodoro/formats/review/plugins/medical_data.rb
+++ lib/pomodoro/formats/review/plugins/medical_data.rb
@@ -11,15 +11,53 @@ module Pomodoro::Formats::Review::Plugins::MedicalData
   def self.parse(string)
     tree = Parser.new.parse(string)
     nodes = Transformer.new.apply(tree)
-    Expenses.new(nodes.empty? ? Array.new : nodes)
+    nodes.empty? ? Array.new : nodes
   end
 
   class Parser < Parslet::Parser
-    # TODO
+    rule(:type) {
+      (str(':').absent? >> any).repeat.as(:sym) >> str(':')
+    }
+
+    rule(:description) {
+      (str("\n").absent? >> any).repeat.as(:str) >> str("\n")
+    }
+
+    rule(:note_line) {
+      str('  ') >> (str("\n").absent? >> any).repeat(1).as(:str) >> str("\n")
+    }
+
+    rule(:node) {
+      type.as(:type) >> description.as(:description) >> note_line.repeat.as(:notes)
+    }
+
+    rule(:nodes) {
+      node.as(:node).repeat
+    }
+
+    root(:nodes)
+  end
+
+  class MedicalEvent
+    TYPES ||= [
+      :weight, :incident, :blood_pressure, :blood_glucose
+    ]
+
+    attr_reader :type, :description, :notes
+    def initialize(type:, description:, notes: Array.new)
+      @type, @description, @notes = type, description, notes
+      unless TYPES.include?(type)
+        raise ArgumentError.new("Unknown type: #{type}")
+      end
+    end
   end
 
   class Transformer < Parslet::Transform
-    # TODO
+    rule(str: simple(:slice)) { slice.to_s.strip }
+    rule(sym: simple(:slice)) { slice.to_s.strip.downcase.tr(' ', '_').to_sym }
+    rule(node: subtree(:data)) {
+      MedicalEvent.new(**data)
+    }
   end
 end
 

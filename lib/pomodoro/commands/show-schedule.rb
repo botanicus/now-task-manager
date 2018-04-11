b4818e270458504565f84dfe9b5e55ecd1bff34c
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2018-04-11 13:22:22 +0200

    show-schedule Saturday

--- lib/pomodoro/commands/show-schedule.rb
+++ lib/pomodoro/commands/show-schedule.rb
@@ -1,15 +1,33 @@
 require_relative 'generate'
 
+module DateTimeMixin
+  def next_week
+    self + (7 - self.wday)
+  end
+
+  def next_wday(n)
+    n > self.wday ? self + (n - self.wday) : self.next_week.next_day(n)
+  end
+end
+
 class Pomodoro::Commands::ShowSchedule < Pomodoro::Commands::Generate
   self.help = <<-EOF.gsub(/^\s*/, '')
     now show-schedule
     now show-schedule +1
     now show-schedule holidays
+    now show-schedule Saturday
   EOF
 
   def run
-    @date = Date.today + self.get_date_increment
-    options = self.group_args
+    if @args & Date::DAYNAMES
+      day_name = (@args & Date::DAYNAMES).first
+      wday  = Date::DAYNAMES.index(day_name)
+      @date = Date.today.extend(DateTimeMixin).next_wday(wday)
+      options = Hash.new ###
+    else
+      @date = Date.today + self.get_date_increment
+    end
+
     scheduler = self.get_scheduler(@date)
     schedule = self.get_schedule(scheduler, **options)
     day = schedule.call

45b026ed7112090249461fdc02a22b5c0cee2086
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2018-05-01 18:20:57 -0500

    Fix.

--- lib/pomodoro/commands/show-schedule.rb
+++ lib/pomodoro/commands/show-schedule.rb
@@ -6,6 +6,10 @@ module DateTimeMixin
   end
 
   def next_wday(n)
+    unless n.is_a?(Integer)
+      raise ArgumentError.new("DateTimeMixin#next_wday expects Integer, got #{n.class}.")
+    end
+
     n > self.wday ? self + (n - self.wday) : self.next_week.next_day(n)
   end
 end
@@ -19,13 +23,20 @@ class Pomodoro::Commands::ShowSchedule < Pomodoro::Commands::Generate
   EOF
 
   def run
-    if @args & Date::DAYNAMES
+    if Date::DAYNAMES.include?(@args.first)
+      # now show-schedule Saturday
       day_name = (@args & Date::DAYNAMES).first
       wday  = Date::DAYNAMES.index(day_name)
       @date = Date.today.extend(DateTimeMixin).next_wday(wday)
-      options = Hash.new ###
-    else
+      options = Hash.new
+    elsif @args[0].match(/^\+\d+$/)
+      # now show-schedule +1
       @date = Date.today + self.get_date_increment
+      options = Hash.new
+    else
+      # now show-schedule holidays
+      @date = Date.today + 1
+      options = group_args.slice(:schedule)
     end
 
     scheduler = self.get_scheduler(@date)

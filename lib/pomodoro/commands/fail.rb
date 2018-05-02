e832460f03a87c0524d5328769c1a00fb3af99ab
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2018-05-02 11:44:11 -0500

    Fail specs.

--- lib/pomodoro/commands/fail.rb
+++ lib/pomodoro/commands/fail.rb
@@ -1,3 +1,4 @@
+# 1/5/2018: specs complete, help complete.
 class Pomodoro::Commands::Fail < Pomodoro::Commands::Command
   self.help = <<-EOF.gsub(/^\s*/, '')
     now <magenta>fail</magenta> <bright_black># #{self.description}</bright_black>
@@ -8,13 +9,13 @@ class Pomodoro::Commands::Fail < Pomodoro::Commands::Command
 
     if  with_active_task(self.config) do |active_task|
           print "#{t(:prompt_why)} "
-          reason = STDIN.readline.chomp
+          reason = $stdin.readline.chomp
 
           active_task.fail!(reason)
           puts t(:success, task: Pomodoro::Tools.unsentence(active_task.body))
         end
     else
-      abort "<red>There is no task in progress.</red>" # FIXME.
+      abort "<red>There is no task in progress.</red>"
     end
   rescue Pomodoro::Config::ConfigError => error
     abort error

04c2b6aabf1e99ec54ef8e6a27f5a102d433659f
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2018-05-02 11:34:28 -0500

    Edit.

--- lib/pomodoro/commands/edit.rb
+++ lib/pomodoro/commands/edit.rb
@@ -1,4 +1,3 @@
-# 1/5/2018: specs complete, help complete.
 class Pomodoro::Commands::Edit < Pomodoro::Commands::Command
   self.help = <<-EOF.gsub(/^\s*/, '')
     now <green>e</green>                   <bright_black># Edit the today task file.</bright_black>
@@ -8,22 +7,24 @@ class Pomodoro::Commands::Edit < Pomodoro::Commands::Command
   EOF
 
   def run
-    if @args.empty?
+    case @args.first
+    when nil
       self.ensure_today; command("vim #{self.config.today_path}")
-    elsif @args.first.to_i == 2 # This could also be tomorrow + tasks, not just today + tasks.
+    when '2'
+      # This could also be tomorrow + tasks, not just today + tasks.
       self.ensure_today; self.ensure_task_list
       command("vim -O2 #{self.config.today_path} #{self.config.task_list_path}")
-    elsif @args.first == '+1'
+    when '+1'
       tomorrow = Date.today + 1; self.ensure_today(tomorrow)
       command("vim #{self.config.today_path(tomorrow)}")
-    elsif ['tasks', 't'].include?(@args.first)
+    when 'tasks', 't'
       self.ensure_task_list; command("vim #{self.config.task_list_path}")
-    elsif ['config', 'c'].include?(@args.first)
+    when 'config', 'c'
       command("vim #{self.config.path}") # TODO: spec
     else
       abort(self.class.help)
     end
   rescue Pomodoro::Config::ConfigError => error
-    abort error
+    abort error # TODO: Do we want to redefine abort to format exceptions?
   end
 end
